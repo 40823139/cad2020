ab4706c49d7ce598cbdbb4479b4d5d330798f0c9
40823139 <40823139@gm.nfu.edu.tw>
2021-01-14 12:04:32 +0800

    last modify003

---
+++ downloads/inventor.dart
@@ -0,0 +1,31 @@
+int s=1;
+int i;
+int start = 1;
+int end = 64;
+int sum;
+//長
+int L =50;
+//寬
+int W = 60;
+//高
+int H = 70;
+int d3 = 0;
+int d12 =10;
+main(){
+  sum = 0;
+  for(i=start;i <= end ;i++){
+    L=L+5;
+    W=W+5;
+    H=H+5;
+    s=s+1;
+    d3=d3+0;
+    d12=d12+5;
+    print("If Style = $s Then \n Leight = $L");
+    print("Width = $W \n Height = $H");
+    print("d3 = $d3 \n d12 = $d12");
+    print("volume = iProperties.Volume \n Volume = volume \n Parameter.UpdateAfterChange = True");
+    print("End If");
+
+  }
+  return 0;
+}
